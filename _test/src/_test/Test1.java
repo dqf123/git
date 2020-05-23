9bc1f6f8415652596b117590306adf0500b0231d
dqf123 <59460057+dqf123@users.noreply.github.com>
2020-05-24 00:05:25 +0800

    Update Test1.java

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -2,17 +2,13 @@ package _test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		int a =10;
-		int b =20;
-		int c =a+b;
-
+		int c=11;
+		intb=88;
 		String name ="赵云";
-
-		System.out.println(a);
-		System.out.println(b);
+     
 		System.out.println(c);
-		System.out.println(name);
-
+		System.out.println(b);
+		
 
 
 
