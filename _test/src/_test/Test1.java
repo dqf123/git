e97b638d5064c38c0ad9f4615471de5f5c064701
dqf123 <59460057+dqf123@users.noreply.github.com>
2020-05-23 23:18:29 +0800

    Update Test1.java

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -4,10 +4,12 @@ public class Test1 {
    public static void main(String[] args) {
 	   int a =10;
 
-	String b="abc";
+	   int b =20;
+	   int c =a+b;
+	   
 	System.out.println(a);
 	System.out.println(b);
-	System.out.println(a+b);
+	System.out.println(c);
 	
 
 
