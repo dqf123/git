2d8848612ddbae1bedbb7932908e225d22ce6857
dqf123 <59460057+dqf123@users.noreply.github.com>
2020-05-25 19:41:37 +0800

    Update Test1.java

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 
 		int a = 1 ;
+		int b = 2 ;
 		String name ="1";
 		String name1 ="2";
 		String name2="3";
@@ -12,7 +13,7 @@ public class Test1 {
 		System.out.println(name);
 		System.out.println(name1);
 		System.out.println(name2);
-
+                System.out.println(b);
 
 	}
 }
