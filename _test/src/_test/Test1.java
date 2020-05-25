8b406358d517cc1595eb0332fe4143770647f699
dqf123 <59460057+dqf123@users.noreply.github.com>
2020-05-25 20:02:29 +0800

    Update Test1.java

--- _test/src/_test/Test1.java
+++ _test/src/_test/Test1.java
@@ -4,24 +4,9 @@ public class Test1 {
 	public static void main(String[] args) {
 
 		int a = 1 ;
-		int b = 2 ;
-
-		String name ="1";
-		String name1 ="唐僧";
-		String name8 ="孙悟空";
-		String name66 ="猪八戒";
-
-	
-
-		String name2="3";
-		String name3="赵云";
+		
 
 		System.out.println(a);
-		System.out.println(name);
-		System.out.println(name1);
-		System.out.println(name2);
-
-		System.out.println(name3);
 
 
        System.out.println(b);
